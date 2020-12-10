c27d1261354f7ecb39dd1bd4bc4f261a18a2b235
Shaletanu <shaletanu0@gmail.com>
2020-12-10 19:22:52 +0530

    Initial commit

---
+++ Problem1.py
@@ -0,0 +1,18 @@
+arr = []
+size = int(input("Enter size: "))
+for i in range(0, size) :
+    b = int(input("Enter list item: "))
+    arr.append(b)
+print(arr)
+
+
+def FindPair(arr, k):
+    for i in range(0, len(arr)) :
+        for j in range(0, len(arr)):
+            if k == arr[i] + arr[j]:
+                return True
+    return False
+
+
+k = int(input("Enter Number: "))
+print(FindPair(arr, k))
